fb1ad7787e7dd3a6f9400404867a83ea99a4b8a6
Ryan Mitchell <remitche@indiana.edu>
2019-05-29 12:51:18 -0400

    Minor updates to clean the code.

--- FSEEData/FSEEDataSetList.C
+++ FSEEData/FSEEDataSetList.C
@@ -5,22 +5,22 @@
 #include <utility>
 #include "TChain.h"
 #include "TString.h"
-#include "FSData/FSDataSet.h"
-#include "FSData/FSDataSetList.h"
+#include "FSEEData/FSEEDataSet.h"
+#include "FSEEData/FSEEDataSetList.h"
 #include "FSBasic/FSString.h"
 
 
-vector<FSDataSet*> FSDataSetList::m_vectorDataSets;
-map< TString, FSDataSet* > FSDataSetList::m_mapTempDataSets;
+vector<FSEEDataSet*> FSEEDataSetList::m_vectorDataSets;
+map< TString, FSEEDataSet* > FSEEDataSetList::m_mapTempDataSets;
 
 
 void
-FSDataSetList::addDataSetsFromFile(TString fileName){
+FSEEDataSetList::addDataSetsFromFile(TString fileName){
   clearTempDataSets();
-  cout << "FSDataSetList: Reading data from file: " << fileName << endl;
+  cout << "FSEEDataSetList: Reading data from file: " << fileName << endl;
   ifstream infile(FSString::TString2string(fileName).c_str());
   if (!infile.is_open()){
-    cout << "FSDataSetList ERROR: cannot find file: " << fileName << endl;
+    cout << "FSEEDataSetList ERROR: cannot find file: " << fileName << endl;
     exit(0);
   }
   string sline;
@@ -31,12 +31,12 @@ FSDataSetList::addDataSetsFromFile(TString fileName){
     if (words[0][0] == '#') continue;
     if (words[0] == "data"){
       if (words.size() < 10){
-        cout << "FSDataSetList ERROR: problem with data command" << endl;
+        cout << "FSEEDataSetList ERROR: problem with data command" << endl;
         exit(0);
       }
       TString fsName(words[1]);
         if (getDataSetVector(fsName).size() > 0){
-          cout << "FSDataSetList ERROR:  using the same data set name twice: " << fsName <<  endl;
+          cout << "FSEEDataSetList ERROR:  using the same data set name twice: " << fsName <<  endl;
           exit(1);
         }
       int fsRunStart(FSString::TString2int(words[2]));
@@ -73,7 +73,7 @@ FSDataSetList::addDataSetsFromFile(TString fileName){
         if (relativeErrorSyst) fsLumSystError = fsLumSystError*fsLum;
       vector<TString> fsCategories;
         for (unsigned int i = 10; i < words.size(); i++){ fsCategories.push_back(words[i]); }
-      FSDataSet* fsd = new FSDataSet(fsName, fsRunStart, fsRunEnd,
+      FSEEDataSet* fsd = new FSEEDataSet(fsName, fsRunStart, fsRunEnd,
                                      fsEcm, fsEcmStatError, fsEcmSystError,
                                      fsLum, fsLumStatError, fsLumSystError, fsCategories);
       fsd->display();
@@ -81,41 +81,41 @@ FSDataSetList::addDataSetsFromFile(TString fileName){
     }
     else if (words[0] == "category"){
       if (words.size() < 3){
-        cout << "FSDataSetList ERROR: problem with cateory command" << endl;
+        cout << "FSEEDataSetList ERROR: problem with cateory command" << endl;
         exit(0);
       }
       TString category = words[1];
       for (unsigned int i = 2; i < words.size(); i++){
-        vector<FSDataSet*> fsdv = getDataSetVector(words[i]);
+        vector<FSEEDataSet*> fsdv = getDataSetVector(words[i]);
         for (unsigned int idata = 0; idata < fsdv.size(); idata++){
-          fsdv[idata]->addCategory(category);
+          fsdv[idata]->addDSCategory(category,false);
         }
       }
     }
     else{
-      cout << "FSDataSetList ERROR: problem reading file " << fileName << endl;
+      cout << "FSEEDataSetList ERROR: problem reading file " << fileName << endl;
       cout << "  line:" << endl << line << endl;  exit(1);
     }
   }
   if (m_vectorDataSets.size() == 0){
-    cout << "FSDataSetList WARNING:  no points found in file: " << fileName << endl;
+    cout << "FSEEDataSetList WARNING:  no points found in file: " << fileName << endl;
   }
 }
 
 
 
 
-FSDataSet*
-FSDataSetList::getDataSet(TString category, double ecmGrouping){
+FSEEDataSet*
+FSEEDataSetList::getDataSet(TString category, double ecmGrouping){
   TString name = "(total)" + category;
   if (ecmGrouping > 0.0){
     name += "(ecmgroup)" + FSString::double2TString(ecmGrouping,8,true);
   }
-  FSDataSet* fsd = findTempDataSet(name);
+  FSEEDataSet* fsd = findTempDataSet(name);
   if (fsd) return fsd;
-  vector<FSDataSet*> fsdv = getDataSetVector(category,ecmGrouping);
+  vector<FSEEDataSet*> fsdv = getDataSetVector(category,ecmGrouping);
   if (fsdv.size() == 0) return NULL;
-  fsd = new FSDataSet(name);
+  fsd = new FSEEDataSet(name);
   for (unsigned int i = 0; i < fsdv.size(); i++){
     fsd->addSubSet(fsdv[i]);
   }
@@ -125,12 +125,12 @@ FSDataSetList::getDataSet(TString category, double ecmGrouping){
 
 
 
-vector<FSDataSet*>
-FSDataSetList::getDataSetVector(TString category, double ecmGrouping){
+vector<FSEEDataSet*>
+FSEEDataSetList::getDataSetVector(TString category, double ecmGrouping){
     // loop over data sets and keep those that pass the category logic
-  vector<FSDataSet*> fsdVectorSelected;
+  vector<FSEEDataSet*> fsdVectorSelected;
   for (unsigned int i = 0; i < m_vectorDataSets.size(); i++){
-    FSDataSet* fsd = m_vectorDataSets[i];
+    FSEEDataSet* fsd = m_vectorDataSets[i];
     if (FSString::evalLogicalTString(category,fsd->categories()))
       fsdVectorSelected.push_back(fsd);
   }
@@ -145,28 +145,28 @@ FSDataSetList::getDataSetVector(TString category, double ecmGrouping){
     TString namej = fsdVectorSelected[j]->name();
     if ((ecmi > ecmj) || 
         ((ecmi == ecmj) && (namei > namej))){
-      FSDataSet* tempDataSet = fsdVectorSelected[i];
+      FSEEDataSet* tempDataSet = fsdVectorSelected[i];
       fsdVectorSelected[i] = fsdVectorSelected[j];
       fsdVectorSelected[j] = tempDataSet;
     }
   }}
     // do the ecm grouping
-  vector<FSDataSet*> fsdVectorGrouped;
+  vector<FSEEDataSet*> fsdVectorGrouped;
   for (unsigned int i = 0; i < fsdVectorSelected.size(); i++){
-    TString groupCat(fsdVectorSelected[i]->name());
+    TString groupCat = "(" + fsdVectorSelected[i]->name() + ")";
     double ecmi = fsdVectorSelected[i]->ecm();
     int iskip = 0;
     for (unsigned int j = i+1; j < fsdVectorSelected.size(); j++){
       double ecmj = fsdVectorSelected[j]->ecm();
       if ((ecmj - ecmi) < ecmGrouping){
-        groupCat += "," + fsdVectorSelected[j]->name();
+        groupCat += ",(" + fsdVectorSelected[j]->name() + ")";
         iskip++;
       }
       else{ break; }
     }
-    FSDataSet* fsdGroup = getDataSet(groupCat);
-    if (!fsdGroup){ cout << "FSDataSetList ERROR" << endl; exit(1); }
-    vector<FSDataSet*> groupSubSets = fsdGroup->subSets();
+    FSEEDataSet* fsdGroup = getDataSet(groupCat);
+    if (!fsdGroup){ cout << "FSEEDataSetList ERROR" << endl; exit(1); }
+    vector<FSEEDataSet*> groupSubSets = fsdGroup->subSets();
     if (groupSubSets.size() == 1){
       fsdVectorGrouped.push_back(groupSubSets[0]);
     }
@@ -175,25 +175,19 @@ FSDataSetList::getDataSetVector(TString category, double ecmGrouping){
     }
     i += iskip;
   }
-  // check for nearby points
-  // FSDatSet* first, last
-  // if gaps too big, error, ambiguity
-
-  // use getDataSet to combine
-
+  // if gaps too big, error, ambiguity?
  return fsdVectorGrouped;
-
 }
 
 
 void 
-FSDataSetList::display(TString category, double ecmGrouping){
-  FSDataSet* fsd = getDataSet(category,ecmGrouping);
+FSEEDataSetList::display(TString category, double ecmGrouping){
+  FSEEDataSet* fsd = getDataSet(category,ecmGrouping);
   if (!fsd){
     cout << "No data sets found for category: " << category << endl;
     return;
   }
-  vector<FSDataSet*> fsdv = fsd->subSets();
+  vector<FSEEDataSet*> fsdv = fsd->subSets();
   cout << endl << "****** SUB DATA SETS ******" << endl << endl;
   for (unsigned int i = 0; i < fsdv.size(); i++){
     fsdv[i]->display(i+1);
@@ -204,18 +198,18 @@ FSDataSetList::display(TString category, double ecmGrouping){
 
 
 
-FSDataSet*
-FSDataSetList::findTempDataSet(TString name){
-  map<TString,FSDataSet*>::iterator it = m_mapTempDataSets.find(name);
+FSEEDataSet*
+FSEEDataSetList::findTempDataSet(TString name){
+  map<TString,FSEEDataSet*>::iterator it = m_mapTempDataSets.find(name);
   if (it != m_mapTempDataSets.end()) return (*it).second;
-  FSDataSet* fsd = NULL;
+  FSEEDataSet* fsd = NULL;
   return fsd;
 }
 
 
 void
-FSDataSetList::clearTempDataSets(){
-  for (map<TString,FSDataSet*>::iterator mapitr = m_mapTempDataSets.begin();
+FSEEDataSetList::clearTempDataSets(){
+  for (map<TString,FSEEDataSet*>::iterator mapitr = m_mapTempDataSets.begin();
        mapitr != m_mapTempDataSets.end(); mapitr++){
     if ((*mapitr).second) delete (*mapitr).second;
   }
@@ -224,7 +218,7 @@ FSDataSetList::clearTempDataSets(){
 
 
 void
-FSDataSetList::clearDataSets(){
+FSEEDataSetList::clearDataSets(){
   clearTempDataSets();
   for (unsigned int i = 0; i < m_vectorDataSets.size(); i++){
     if (m_vectorDataSets[i]) delete m_vectorDataSets[i];
--- FSEEData/FSEEDataSetList.h
+++ FSEEData/FSEEDataSetList.h
@@ -1,23 +1,23 @@
-#if !defined(DATASETLIST_H)
-#define DATASETLIST_H
+#if !defined(FSEEDATASETLIST_H)
+#define FSEEDATASETLIST_H
 
 #include <utility>
 #include <map>
 #include <vector>
 #include "TString.h"
-#include "FSData/FSDataSet.h"
+#include "FSEEData/FSEEDataSet.h"
 
 
 using namespace std;
 
 
-class FSDataSetList{
+class FSEEDataSetList{
 
   public:
 
     static void addDataSetsFromFile(TString fileName);
 
-    static FSDataSet* getDataSet(TString category = "", double ecmGrouping = -1.0);
+    static FSEEDataSet* getDataSet(TString category = "", double ecmGrouping = -1.0);
 
     static void display(TString category = "", double ecmGrouping = -1.0);
 
@@ -26,12 +26,12 @@ class FSDataSetList{
   private:
 
     static void clearTempDataSets();
-    static FSDataSet* findTempDataSet(TString name);
+    static FSEEDataSet* findTempDataSet(TString name);
 
-    static vector<FSDataSet*> getDataSetVector(TString category = "", double ecmGrouping = -1.0);
+    static vector<FSEEDataSet*> getDataSetVector(TString category = "", double ecmGrouping = -1.0);
 
-    static vector<FSDataSet*> m_vectorDataSets;
-    static map< TString, FSDataSet* > m_mapTempDataSets;
+    static vector<FSEEDataSet*> m_vectorDataSets;
+    static map< TString, FSEEDataSet* > m_mapTempDataSets;
 
 };
 
