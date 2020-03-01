f11d6e1114a1824f0f7eac1e196f39ddf94b4c78
Ryan Mitchell <remitche@indiana.edu>
2020-03-01 15:59:39 -0500

    Be more careful adding friend trees.

--- FSBasic/FSTree.C
+++ FSBasic/FSTree.C
@@ -90,25 +90,30 @@ FSTree::getTChain(TString fileName, TString ntName,
     }
   }
 
+    // add a friend to the tree
 
   if ((nt) && (FSControl::CHAINFRIEND != "")){
-
-    TString friendFileName(fileName);
-    friendFileName += ".";
-    friendFileName += FSControl::CHAINFRIEND;
-
-    TString friendNTName(ntName);
-    friendNTName += "_";
-    friendNTName += FSControl::CHAINFRIEND;
-
+      // get the list of files for this chain (order matters)
+    vector<TString> fileList;
+    TIter next(nt->GetListOfFiles());
+    while ( TChainElement* el = (TChainElement*) next()){
+      fileList.push_back(el->GetTitle());
+    }
+      // loop over files individually and add the friend tree
+    for (unsigned int i = 0; i < fileList.size(); i++){
+      TString friendFileName(fileList[i]);
+      friendFileName += ".";
+      friendFileName += FSControl::CHAINFRIEND;
+      TString friendNTName(ntName);
+      friendNTName += "_";
+      friendNTName += FSControl::CHAINFRIEND;
       if (FSControl::DEBUG){
-	cout << "FSTree: adding friend tree... " << endl;
+        cout << "FSTree: adding friend tree... " << endl;
         cout << "            friend tree name = " << friendNTName << endl;
         cout << "            friend tree file = " << friendFileName << endl;
       }
-
-    nt->AddFriend(friendNTName,friendFileName);
-
+      nt->AddFriend(friendNTName,friendFileName);
+    }
   }
 
 
