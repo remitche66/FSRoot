21ab3b01464a3157a1bc6400015ce9c03de203c9
Ryan Mitchell <remitche@indiana.edu>
2020-07-22 17:19:46 -0400

    Version 2.4+

--- rootlogon.FSROOT.C
+++ rootlogon.FSROOT.C
@@ -1,7 +1,7 @@
 {
 
 cout << "--------------------------------------------------" << endl;
-cout << "------   Loading the FSRoot Macros (v2.4)   ------" << endl;
+cout << "------   Loading the FSRoot Macros (v2.4+)  ------" << endl;
 cout << "--------------------------------------------------" << endl;
 
 
