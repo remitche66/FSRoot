3cfa9be1c6635b2aa115a568bf6fd70440c88458
Ryan Mitchell <remitche@indiana.edu>
2025-01-07 12:06:20 -0500

    Make a pdf in ExampleProgram.cc

--- Examples/Program/ExampleProgram.cc
+++ Examples/Program/ExampleProgram.cc
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "TCanvas.h"
 #include "FSBasic/FSString.h"
+#include "FSBasic/FSHistogram.h"
 
 using namespace std;
 
@@ -8,6 +10,11 @@ int main(){
   cout << "testing!" << endl;
 
   cout << FSString::double2TString(4.4444) << endl;
+
+  TCanvas* c1 = new TCanvas("c1","c1",500,500);
+  TH1F* hist = FSHistogram::getTH1FFormula("x+1","(100,0.0,1.0)");
+  hist->Draw();
+  c1->Print("testing.pdf");
 
 }
 
